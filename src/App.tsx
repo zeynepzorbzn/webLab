0a25a7801751f672c4902ef4770ffab324734191
zeynepnzorbzn <147322358+zeynepnzorbzn@users.noreply.github.com>
2026-02-17 14:47:41 +0300

    chore : initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/electron-vite.animate.svg'
+import './App.css'
+
+
+
+function App () {
+   return (
+   <div >
+  <h1 >Web Tasarimi ve Programlama </h1 >
+   <h2 >LAB -1 </h2 >
+   <p>Ad Soyad : [ Zeynep Nur Zorbozan ]</p>
+   <p> Ogrenci No: [ 235541085 ]</p>
+   </div >
+   )
+   }
+  
+   export default App
